89ecf7c1ac370a85863b2a9cd524208b1cd6e559
Fguimaraes12 <guimaraesfelipe6021@gmail.com>
2026-06-29 20:14:15 -0300

    style: style header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,8 @@
 
 import { useCartContext } from "@/context/cartContext";
 import Cart from "../cart/cart";
+import { ShoppingCart, Sparkles, User } from "lucide-react";
+import LinesEffect from "./linesEffect";
 
 
 
@@ -9,25 +11,35 @@ export default function Header(){
 const {modalCart, setModalCart} = useCartContext()
 
     return(
-        <div className="w-full h-20 flex items-center justify-evenly border border-green-500">
+   <div>
+    <LinesEffect/>
+            <div className="w-full h-20 flex items-center justify-evenly">
             {/* LOGO */}
-            <div> 
+            <div className="flex items-center"> 
+                <Sparkles size={25} strokeWidth={1} color="green"/>
                <h1 className="text-2xl">Store</h1>
             </div>
 
-            <div className="flex gap-3">
-                <button className="cursor-pointer">Shop</button>
-                 <button className="cursor-pointer">Collections</button>
-                 <button className="cursor-pointer">Explore</button>
+            <div className="flex gap-7">
+                <button className="cursor-pointer btn-underline">Shop</button>
+                 <button className="cursor-pointer btn-underline">Collections</button>
+                 <button className="cursor-pointer btn-underline">Explore</button>
             </div>
 
 
             {/* BUTTON CART */}
             <div className="flex gap-3">
-              <button onClick={() => setModalCart(!modalCart)} className="cursor-pointer">Cart</button>
-              <span className="cursor-pointer">My account</span>
+              <div onClick={() => setModalCart(!modalCart)} className="flex items-center gap-1 cursor-pointer">
+                <ShoppingCart size={16} strokeWidth={2} />
+                <button className="cursor-pointer">Cart</button>
+              </div>
+              <div className="flex items-center gap-1">
+                <User size={16} strokeWidth={2}/>
+                <span className="cursor-pointer">My account</span>
+              </div>
               {modalCart && <Cart/>}
             </div>
         </div>
+   </div>
     )
 }
