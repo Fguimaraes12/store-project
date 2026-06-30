a0df0b1f99320a1271d436cf4b38e3b8102e5585
Fguimaraes12 <guimaraesfelipe6021@gmail.com>
2026-06-30 00:16:08 -0300

    feat(hero): implement hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <div>
        <Header/>
+       <Hero/>
        <FilterBar/>
        <ProductsList/>
     </div>
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Hero(){
+    return(
+         <div className="flex flex-col gap-2 ml-75 my-30">
+            <h1 className="text-7xl">All in One Place</h1>
+            <p className="text-2xl font-extralight max-w-230">Whatever you're looking for, we've got it. Browse our collections and discover something new every day.</p>
+        </div>
+    )
+}
